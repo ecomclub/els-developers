0b14dcefcc53ce9116b8e4e5eeda2aacf891d304
Talisson Trindade <talissonf@gmail.com>
2018-08-30 12:14:46 -0300

    change in function isMd()

--- build/main.php
+++ build/main.php
@@ -7,7 +7,6 @@
 class GitToEls
 {
     private $repoToFind = [];
-    private $gitPath = 'https://api.github.com/repos/ecomclub/';
     private $pagination = 1;
     private $data;
     private $dir;
@@ -84,7 +83,7 @@ class GitToEls
      */
     public function isMD($file)
     {
-        return substr($file, -3) === ".md" ? true : false;
+        return ($file[0] != '.' && substr($file, -3) === '.md') ? true : false;
     }
 
     /**
@@ -108,9 +107,10 @@ class GitToEls
         }
     }
 }
-
-// using
+/*
+Using
 $repos = ["ecomplus-sdk-js","ecomplus-store-render","ecomplus-search-api-docs","ecomplus-api-docs","ecomplus-store-template","ecomplus-graphs-api-docs","storage-api","storefront-app","ecomplus-passport","ecomplus-passport-client","webhooks-queue","modules-api","ecomplus-neo4j"];
 $git = new GitToEls($repos);
 $git->search();
 $git->save();
+*/
